0620cc24fe65d7c76c5d2a0d878d576410133d4a
Inna Antypova <nightallatiel@gmail.com>
2023-12-10 08:50:19 +0300

    fix

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from catalog.views import index
+
+
+from catalog.views import index, contacts
 
 urlpatterns = [
-    path('', index)
+    path('home/', index),
+    path('contacts/', contacts),
 ]
