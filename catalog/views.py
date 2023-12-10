6ba56cee7b663c64662f49cd99ec80333eed68af
Inna Antypova <nightallatiel@gmail.com>
2023-12-10 08:50:44 +0300

    fix

--- catalog/views.py
+++ catalog/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'catalog/index.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'Пришло сообщение:\nИмя: {name}\nТел.: {phone}\nEmail: {email}\nСообщение: {message}')
+    return render(request, 'catalog/contacts.html')
+
+
