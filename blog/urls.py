7759e43c4bc000e88ce27ba026dd0cbd1b42bece
Inna Antypova <nightallatiel@gmail.com>
2023-12-22 21:00:26 +0300

    add conrollers

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 
-from catalog.apps import CatalogConfig
+from blog.views import ArticleListView, ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
+from blog.apps import BlogConfig
 
-app_name = CatalogConfig.name
+app_name = BlogConfig.name
 
 urlpatterns = [
-    # path('home/', IndexView.as_view(), name='index'),
-    # path('contacts/', ContactsView.as_view(), name='contacts'),
-    # path('category/', CategoryListView.as_view(), name='category'),
-    # path('<int:pk>/products/', ProductsListView.as_view(), name='products'),
-    # path('<int:pk>/product_detail/', ProductDetailView.as_view(), name='product_detail'),
+    path('', ArticleListView.as_view(), name='article_list'),
+    path('create/', ArticleCreateView.as_view(), name='create_article'),
+    path('detail/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('update/<int:pk>/', ArticleUpdateView.as_view(), name='article_update'),
+    path('delete/<int:pk>/', ArticleDeleteView.as_view(), name='article_delete'),
     # path('save_product/', ProductCreateView.as_view(), name='save_product')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
-# Create your views here.
+from blog.models import Article
+
+
+class ArticleListView(ListView):
+    model = Article
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+
+    fields = ('title', 'body', 'image')
+    success_url = reverse_lazy('blog:article_list')
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+
+    fields = ('title', 'body', 'image')
+    success_url = reverse_lazy('blog:article_list')
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+
+    success_url = reverse_lazy('blog:article_list')
+
